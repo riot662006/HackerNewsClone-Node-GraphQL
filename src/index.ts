35909ec220ca0301efa2cc7937b51d25547211cf
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-04-29 18:25:25 -0500

    Simple query

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,50 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 const typeDefs = `#graphql
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `;
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
+const links: [Link] = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
 const resolvers = {
-    Query: {
-        info: () => `This is the API for a Hackernews Clone`
-    }
-}
+  Query: {
+    info: () => `This is the API for a Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 startStandaloneServer(server, {
-    listen: { port: 4000 },
+  listen: { port: 4000 },
 }).then(({ url }) => {
-    console.log(`🚀  Server ready at: ${url}`);
-})
+  console.log(`🚀  Server ready at: ${url}`);
+});
