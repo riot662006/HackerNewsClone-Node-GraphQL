1f4530be699f10909f9327e7a7ed5c037b839267
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-04-30 09:02:28 -0500

    Simple mutation and reorganization

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = `#graphql
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { readFileSync } from "fs";
 
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 type Link = {
   id: string;
@@ -34,6 +28,40 @@ const resolvers = {
     feed: () => links,
   },
 
+  Mutation: {
+    post: (parent: any, args: { url: string; description: string }) => {
+      const link = {
+        id: `link-${links.length}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent: any, args: { id: string; url: string; description: string }) => {
+      const linkIndex = links.findIndex((link) => link.id === args.id);
+      if (linkIndex === -1) {
+        throw new Error("Link not found");
+      }
+      const updatedLink = {
+        ...links[linkIndex],
+        url: args.url ?? links[linkIndex].url,
+        description: args.description ?? links[linkIndex].description,
+      };
+      links[linkIndex] = updatedLink;
+      return updatedLink;
+    },
+    deleteLink: (parent: any, args: { id: string }) => {
+      const linkIndex = links.findIndex((link) => link.id === args.id);
+      if (linkIndex === -1) {
+        throw new Error("Link not found");
+      }
+      const deletedLink = links[linkIndex];
+      links.splice(linkIndex, 1);
+      return deletedLink;
+    },
+  },
+
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
@@ -41,8 +69,13 @@ const resolvers = {
   },
 };
 
+// get schema from schema.graphql file
+
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: readFileSync(
+    join(__dirname, "..", "src", "schema.graphql"),
+    "utf-8"
+  ),
   resolvers,
 });
 
